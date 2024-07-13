f12b150c33e800679438b58395729ed40e4050bb
Harsh Bhanderi <hbhanderi148@gmail.com>
2024-07-13 17:08:18 +0530

    chore: Add Vite configuration, Tailwind CSS, and Logo component

---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Logo(){
+    return(
+        <img src="Logo.svg" height="75" width="150"></img>
+    )
+}
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+
+export default function Navbar(){
+    return(
+        <>
+            <div className="object-left-top">
+                <Logo />
+            </div> 
+        </>
+    )
+}
