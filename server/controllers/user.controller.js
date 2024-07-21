34399b78e25f63d52195e5f80adf6c4ca04b1de7
Harsh Bhanderi <hbhanderi148@gmail.com>
2024-07-21 16:43:44 +0530

    chore: Update user controller and auth middleware

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,92 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { APIError } from "../utils/APIError.js";
+import { User } from "../models/user.models.js";
+import { APIResponse } from "../utils/APIResponse.js";
+import jwt from "jsonwebtoken";
+
+const signUpUser = asyncHandler(async (req, res) => {
+    const {username, email, fullname, password} = req.body
+
+    if (
+        [username, email, fullname, password].some((field) =>
+            field?.trim() === ""
+        )
+    ) {
+        throw new APIError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if(existedUser){
+        throw new APIError(409, "User with email or username already exist");
+    }
+
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email,
+        fullname,
+        password
+    });
+
+    const createdUser = await User.findById(user._id).select( // delete password and refreshToken while returning user to frontend
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new APIError(500, "Internal server error")
+    }
+
+    return res.status(201).json(
+        new APIResponse(200, createdUser, "User registered successfully")
+    )
+});
+
+const signInUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body
+
+    if(!username && !email){
+        throw new APIError(400, "username and email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [
+            {username}, {email}
+        ]
+    })
+    if(!user){
+        throw new APIError(404, "user does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new APIError(401, "Password is incorrect")
+    }
+
+    const { refreshToken, accessToken } = await generateAccessAndRefreshToken(user._id);
+    
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new APIResponse(
+            200, 
+            {
+                user: loggedInUser, 
+                accessToken: accessToken, 
+                refreshToken: refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+})
