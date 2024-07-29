c75db447219112ff3ef8ede5b1ff41d4010136a8
Henish Trada <tradahenish94@gmail.com>
2024-07-29 11:22:21 +0530

    Added close menu in navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,39 +1,124 @@
 import React from "react";
 import Logo from "./Logo";
+import LogoSymbol from "./LogoSymbol";
 import { Link } from 'react-router-dom'
+import { CheckCircle, ChevronDown, ChevronUp, Menu, Star, X } from 'lucide-react'
 import "./Header.css"
 
+const menuItems = [
+    {
+        name: 'Solution',
+        href: '#',
+    },
+    {
+        name: 'About Us',
+        href: '#',
+    },
+    {
+        name: 'FAQ',
+        href: '#',
+    },
+    {
+        name: 'Contact Us',
+        href: '#',
+    },
+]
+
 export default function Navbar() {
+
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return (
         <>
-            <nav className="w-full max-w-6xl h-16 m-auto px-4 py-2 rounded-lg flex items-center justify-between overflow-y-hidden text-base sm:text-sm md:text-tiny lg:text-base" style={{backgroundColor : "rgba(139, 148, 173, 0.51)   "}}>
-                <div className="flex justify-center items-center h-14 px-2 py-1 rounded-md">
-                        <Logo />
+            <nav className="w-5/6 lg:max-w-screen-lg max-w-screen-md h-16 m-auto px-4 py-2 rounded-lg flex items-center justify-between text-base sm:text-sm md:text-tiny lg:text-base " style={{ backgroundColor: "rgba(139, 148, 173, 0.51)" }}>
+                <div className="flex justify-center items-center h-14 px-2 py-1 rounded-md ">
+                    <Logo/>
                 </div>
-                <div className="flex bg-slate-500 h-12 mr-1 my-0.5 px-2 py-1 rounded-lg items-center justify-between w-4/5 text-white navLink" style={{ backgroundColor: "rgba(3, 9, 32, 1)" }}>
-                    <div className="mx-16">
-                        <ul className="flex items-center gap-8 ">
-                        
-                            <li>Soultion</li>
-                            <li className="text-pretty">About Us</li>
-                            <li>FAQ</li>
-                            <li>Contact Us</li>
-
+                <div className="hidden lg:flex bg-slate-500 h-12 mr-1 my-0.5 px-2 py-1 rounded-lg items-center justify-between w-4/5 text-white navLink" style={{ backgroundColor: "rgba(3, 9, 32, 1)" }}>
+                    <div className="mx-20">
+                        <ul className="flex space-x-8">
+                            {menuItems.map((item) => (
+                                <li key={item.name}>
+                                    <a
+                                        href={item.href}
+                                        className="text-sm  hover:text-slate-300"
+                                    >
+                                        {item.name}
+                                    </a>
+                                </li>
+                            ))}
                         </ul>
                     </div>
-                    
 
-                    <div className="flex items-center gap-8 mx-8">
+
+                    <div className=" items-center gap-8 mx-8 ">
 
                         <Link to="/signin" className="flex items-center">
                             <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-2  font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700  dark:focus:ring-blue-800">Sign in</button>
                         </Link>
-                        <Link to="/signup" className="flex items-center">
+                        {/* <Link to="/signup" className="flex items-center">
                             <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-2  font-medium rounded-lg text-sm px-4 py-1 dark:bg-blue-600 dark:hover:bg-blue-700  dark:focus:ring-blue-800">Sign up</button>
-                        </Link>
+                        </Link> */}
 
                     </div>
                 </div>
+                <div className="lg:hidden">
+                    <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
+                </div>
+                {isMenuOpen && (
+                    <div className="navLink absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden" >
+                        <div className="divide-y-2 divide-gray-50 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5" style={{ backgroundColor: "rgba(139, 148, 173)   " }}>
+                            <div className="px-5 pb-6 pt-5">
+                                <div className="flex items-center justify-between">
+                                    <div className="inline-flex items-center space-x-2">
+                                        <span>
+                                            <img src="LogoSymbol.svg" width="60"
+                                                height="60"></img>
+                                        </span>
+                                        <span className="font-bold">Testimonials.io</span>
+                                    </div>
+                                    <div className="-mr-2">
+                                        <button
+                                            type="button"
+                                            onClick={toggleMenu}
+                                            className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                                        >
+                                            <span className="sr-only">Close menu</span>
+                                            <X className="h-6 w-6" aria-hidden="true" />
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="mt-6">
+                                    <nav className="grid gap-y-4">
+                                        {menuItems.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50"
+                                            >
+                                                <span className="ml-3 text-base font-medium text-gray-900">
+                                                    {item.name}
+                                                </span>
+                                            </a>
+                                        ))}
+                                    </nav>
+                                </div>
+                                <Link to="/signin" className="flex items-center">
+                                <button
+                                    type="button"
+                                    className="mt-4 w-full rounded-md bg-blue-700 px-3 py-2 text-sm text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                                >
+                                    Sign In
+                                </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </nav>
         </>
     )
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -5,13 +5,21 @@ import Navbar from '../Header/Navbar';
 const Home = () => {
     return (
         <>
-        <div className="home-container" style={{borderRadius : "0 0 5rem 5em"}} >
+        <div className="home-container" >
             <Navbar/>
             <div className='text-container'>
-            <h1 className="text text-white text-base sm:text-2xl md:text-4xl lg:text-6xl">Get testimonial from your <br/> customer  <span className='text-base spanText sm:text-1xl md:text-3xl lg:text-5.5xl'> → by<br/> professionals</span></h1>
+            <h1 className=" text-white text-base sm:text-xl md:text-4xl lg:text-6xl" style={{
+                fontWeight: '600',
+                fontFamily:'inter'
+            }}>Get testimonial from your <br/> customer  <span className='text-base sm:text-2xl md:text-4xl lg:text-5.5xl' style={{
+                color: 'rgba(255, 255, 255, 0.6)',
+                fontFamily :'Evolventa'
+            }}> → by<br/> professionals</span></h1>
             <button className="text-white bg-blue-700 hover:bg-blue-600 focus:ring-2  font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700  dark:focus:ring-blue-800 mt-6 button">Try FREE now</button>
             </div>
         </div>
+
+        
         </>
     );
 };
